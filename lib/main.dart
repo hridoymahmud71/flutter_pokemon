a1ac242211e3b801a9b10e4e17601f93dd153a88
hridoymahmud71 <hridoymahmud71@gmail.com>
2019-11-27 14:07:21 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(PokemonApp());
+
+class PokemonApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
